4db181dc16bdd17e63c02cac899179ed4203e61b
cervere <bhargav.rrv@gmail.com>
2023-05-02 10:11:11 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+    // initialize state for the data
+    const [data, setData] = useState([]);
+
+    // load data from JSON file
+    useEffect(() => {
+      fetch('https://github.com/jgreis23/MedEd-Bibliometrics-NSGY/blob/277ad81f0c02f322f79cc0e7e8fea295004d5d9f/Data/pubs_from_2013_foi.json')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Received data')
+        setData(data);
+      });
+    })
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <h1>My Data</h1>
+      <span>
+        {data.length} Records
+      </span>
       </header>
     </div>
   );
